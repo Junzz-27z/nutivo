22f19343b4f21706569e0bb72add10a58f6373e1
intancitra-csk <intancitraaa07@gmail.com>
2026-03-25 10:38:05 +0800

    feat: menambahkan file home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nutivo</title>
+</head>
+<body>
+    <header class="flex justify-between items-center px-25 py-6 sticky top-0 bg-white flex-nowrap z-50">
+        <h2 class="text-3xl">Nutivo</h2>
+    </header>
+</body>
+</html>
